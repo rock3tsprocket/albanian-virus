872573dd4398f243a9a779842e3d629e4bd8d1b4
Rock Pie <139387589+rock3tsprocket@users.noreply.github.com>
2024-10-29 15:28:26 +0100

    major overhaul

--- albanian virus.sh
+++ albanian virus.sh
@@ -1,4 +1,21 @@
-#!/bin/sh
-echo Virus Alert !
-echo Hi, I am an Albanian virus but because of poor technology in my country unfortunately I am not able to harm your computer. Please be so kind to delete one of your important files yourself and then forward me to other users. Many thanks for your cooperation! Best regards,Albanian virus
-echo Yes	No	Cancel
+#!/bin/bash
+
+HEIGHT=15
+WIDTH=40
+CHOICE_HEIGHT=4
+TITLE="Virus Alert !"
+MENU="Hi, I am an Albanian virus but because of poor technology in my country unfortunately I am not able to harm your computer. Please be so kind to delete one of your important files yourself and then forward me to other users. Many thanks to your cooperation! Best regards,Albanian virus"
+
+OPTIONS=(1 "Yes"
+         2 "No"
+         3 "Cancel")
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
